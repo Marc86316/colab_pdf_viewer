861a4bed5f76388aed5377e5e1cfd3007e803139
Marcus Chen <115784478+Marc86316@users.noreply.github.com>
2025-04-10 08:37:31 +0800

    Initial commit: colab PDF viewer module

---
+++ test.py
@@ -0,0 +1,5 @@
+from pdf_viewer import display_scrollable_pdf_from_url
+
+pdf_url = "https://raw.githubusercontent.com/Marc86316/DataCamp/main/Courses/Sampling_in_Python/ch3_Relative_error_of_point_estimates.pdf"
+
+display_scrollable_pdf_from_url(pdf_url, width=800, height=450)
